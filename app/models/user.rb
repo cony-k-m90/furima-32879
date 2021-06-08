c6a0b256ee241096fef40c7186a942cde6ee2c40
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-06-08 22:30:48 +0900

    usersテーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # アソシエーションをここに記述
+
+  # emailとpasswordが空だと保存できないバリデーションは標準で用意されている。
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+
+end
