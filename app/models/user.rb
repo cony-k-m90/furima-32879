34e52a31667a3e2ab294862c94b770fbfa921aaa
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-06-19 18:20:20 +0900

    ユーザー管理機能実装のレビュー後修正_1

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,18 @@ class User < ApplicationRecord
   # has_many :items
   # has_many :purchases
 
-  # emailとpasswordが空だと保存できないバリデーションは標準で用意されている。
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-    validates :last_name
-    validates :first_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナを使用してください' } do
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナを使用してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字と数字の両方を含めて設定してください'
 end
