7b7f521053cef34eac5b6af58c73b707f0cece02
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-06-23 23:47:59 +0900

    商品登録画面を表示させる、Gem(ActiveHash)インストール

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @items = Item.new
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,17 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to  :category
   belongs_to  :user
   #has_one     :purchase
 
   validates :name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :shipping_area_id, presence: true
-  validates :days_to_ship_id, presence: true
+  validates :category_id, numericality: { other_than: 0 }
+  validates :condition_id, numericality: { other_than: 0 }
+  validates :delivery_charge_id, numericality: { other_than: 0 }
+  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :days_to_ship_id, numericality: { other_than: 0 }
   validates :price, presence: true
   validates :user, presence: true
   # バリデーションの記述が終わったら with_options で纏める!!
