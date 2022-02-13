8bf97ec27d3a24274764143ded1672bdb0d843ae
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2022-02-13 23:55:13 +0900

    editアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id,
                                  :delivery_charge_id, :shipping_area_id, :days_to_ship_id, :price, :item_image).merge(user_id: current_user.id)
