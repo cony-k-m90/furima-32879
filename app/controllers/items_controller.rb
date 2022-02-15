3305562c74597ae47cf2400cda84e42809e163be
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2022-02-15 18:32:13 +0900

    コントローラーへupdateアクションを追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id,
                                  :delivery_charge_id, :shipping_area_id, :days_to_ship_id, :price, :item_image).merge(user_id: current_user.id)
