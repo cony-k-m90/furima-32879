3c7f8cf7740254d87efbef558d04e2d6411ae0a2
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2022-02-17 22:08:27 +0900

    コントローラ内に複数存在していた@itemの定義をbefore_actionで一つにまとめてコードをリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update ]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if user_signed_in?
       redirect_to action: :index unless @item.user == current_user
     else
@@ -32,7 +31,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -40,8 +38,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id,
                                  :delivery_charge_id, :shipping_area_id, :days_to_ship_id, :price, :item_image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
