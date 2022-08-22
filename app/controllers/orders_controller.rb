4190a6035e97aa50d138bbb4dcbbe99554cd91e4
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2022-08-22 18:09:02 +0900

    payjp決済機能を実装
    テスト用だが、payjpの「公開鍵」を記述したままpushしてしまい問題があった。
    今回のcommitでは、「秘密鍵」のコントローラーへの追記と、「秘密鍵」「公開鍵」を環境変数に書き換た。
    また前回pushしてしまった「公開鍵」が使えない様、payjp側で「公開鍵」を更新した。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,10 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.find(params[:item_id])
     @item_order = ItemOrder.new(item_order_params)
       if @item_order.valid?
+        pay_item
         @item_order.save
         redirect_to root_path
       else
@@ -22,7 +22,16 @@ class OrdersController < ApplicationController
 
   def item_order_params
     # require: バリデーションで指定した値が存在しなければ例外を出す
-    params.require(:item_order).permit(:postal_code, :shipping_area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:item_order).permit(:postal_code, :shipping_area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token], price: @item.price)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: item_order_params[:token],
+      currency:'jpy'
+    )
   end
 
   def move_to_index
