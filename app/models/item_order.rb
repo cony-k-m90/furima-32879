5f4bb09425f98c1f9e22a265baf28dcf6b5186a5
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2022-10-03 21:01:18 +0900

    テストコード修正および追記
    コードレビュー指摘箇所の修正後の対応

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,13 +1,13 @@
 class ItemOrder
   include ActiveModel::Model
   attr_accessor :postal_code, :shipping_area_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id,
-                :order_id, :token, :price
+                :order_id, :token
 
   with_options presence: true do
+    validates :house_number, :token, :user_id, :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-    validates :house_number, :token
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
   end
 
--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ItemOrder, type: :model do
   before do
-    @item_order = FactoryBot.build(:item_order)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @item_order = FactoryBot.build(:item_order, user_id: @user.id, item_id: @item.id )
   end
 
   describe '商品購入フォームから「配送先入力」箇所の保存とpayjpへ決済処理を行う' do
@@ -20,12 +22,6 @@ RSpec.describe ItemOrder, type: :model do
     end
 
     context '「クレジットカード情報入力」の項目に不備があると決済（商品購入）ができないこと' do
-      it 'priceが空では保存ができないこと' do
-        @item_order.price = nil
-        @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Price can't be blank")
-      end
-
       it 'tokenが空では登録できないこと' do
         @item_order.token = nil
         @item_order.valid?
@@ -43,12 +39,11 @@ RSpec.describe ItemOrder, type: :model do
       end
     end
 
-    context '「配送先入力」の項目が保存されない' do
+    context '「配送先入力」の項目に不備があると保存されない' do
       it 'postal_code が空だと保存できないこと' do
         @item_order.postal_code = ''
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Postal code can't be blank",
-                                                            'Postal code is invalid. Include hyphen(-)')
+        expect(@item_order.errors.full_messages).to include("Postal code can't be blank")
       end
       it 'postal_code の半角のハイフンが正しい位置でないと保存できないこと' do
         @item_order.postal_code = '12-3456'
@@ -65,8 +60,13 @@ RSpec.describe ItemOrder, type: :model do
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
-      it 'shipping_area_id を選択していないと保存できないこと' do
+      it 'shipping_area_id が空だと保存できないこと' do
         @item_order.shipping_area_id = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Shipping area can't be blank")
+      end
+      it 'shipping_area_id を選択していないと保存できないこと' do
+        @item_order.shipping_area_id = 0
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include("Shipping area can't be blank")
       end
@@ -93,14 +93,14 @@ RSpec.describe ItemOrder, type: :model do
       it 'phone_number が空だと保存できないこと' do
         @item_order.phone_number = ''
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Phone number can't be blank", 'Phone number is invalid')
+        expect(@item_order.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_number が全角数字では保存ができないこと' do
         @item_order.phone_number = '０９０１２３４５６７８'
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include('Phone number is invalid')
       end
-      it 'phone_number が10桁未満では保存ができないこと' do
+      it 'phone_number が9桁以下では保存ができないこと' do
         @item_order.phone_number = '090123456'
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include('Phone number is invalid')
@@ -110,7 +110,19 @@ RSpec.describe ItemOrder, type: :model do
         @item_order.valid?
         expect(@item_order.errors.full_messages).to include('Phone number is invalid')
       end
+    end
 
+    context '購入履歴(order.rb)に必要な項目に不備があると保存されない' do
+      it 'user_id(購入者)が空だと購入できない' do
+        @item_order.user_id = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_id(購入商品)が空だと購入できない' do
+        @item_order.item_id = nil
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
